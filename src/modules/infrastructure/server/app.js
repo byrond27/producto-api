43a2fc28a220b03a6ab9438919ab2c654203cf65
Byron Dominguez <byrond27@gmail.com>
2020-04-20 03:13:05 +0200

    feature: change folder structure

--- src/modules/infrastructure/server/app.js
+++ src/modules/infrastructure/server/app.js
@@ -4,9 +4,9 @@ var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
 
-var indexRouter = require("./routes/index");
-var usersRouter = require("./routes/users");
-var helloRouter = require("./routes/hello");
+var indexRouter = require("../routes/index");
+var usersRouter = require("../routes/users");
+var helloRouter = require("../../../app/http/hello");
 
 var app = express();
 
